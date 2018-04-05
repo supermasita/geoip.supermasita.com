3f63dcd4d99cf058258621aef6f5e61fbb62faac
root <root@super02.supermasita.com>
2018-04-05 02:26:44 +0000

    code updates and docker

--- geoip.py
+++ geoip.py
@@ -4,6 +4,8 @@ import geoip2.database
 import json
 import ipaddress
 
+geoipDbPath = './GeoLite2-City/GeoLite2-City.mmdb'
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
 
@@ -17,49 +19,45 @@ class MainHandler(tornado.web.RequestHandler):
                  ipaddress.IPv4Address(checkIp)
              except ipaddress.AddressValueError:
                  self.set_status(400)
-                 jsonObject = {}
-                 jsonObject['status'] = 400
-                 jsonObject['description'] = "Not a valid IP"
-                 self.finish(jsonObject)
+                 jsonErrorMsg = { 'status': 400, 'description': 'Not a valid IP'}
+                 self.finish(jsonErrorMsg)
                  return
          else :
              checkIp = self.request.remote_ip 
 
+
          # Load DB
-         reader = geoip2.database.Reader('./GeoLite2-City/GeoLite2-City.mmdb')
+         dbReader = geoip2.database.Reader(geoipDbPath)
+
         
          # Check if exists in DB 
          try:
-             response = reader.city(checkIp)
+             response = dbReader.city(checkIp)
          except geoip2.errors.AddressNotFoundError:
              self.set_status(400)
-             jsonObject = {}
-             jsonObject['status'] = 400
-             jsonObject['description'] = "IP not found in GeoLite2 database"
-             self.finish(jsonObject)
+             jsonErrorMsg = { 'status': 400, 'description': 'IP not found in GeoLite2 database'}
+             self.finish(jsonErrorMsg)
              return
+         finally:
+             dbReader.close()
 
-         # IP info	
-         jsonObject = json.dumps(response, default=lambda o: o.__dict__)
-         jsonObject = vars(response)
-         del jsonObject['registered_country']
-         del jsonObject['represented_country']
-         del jsonObject['traits']
-         del jsonObject['raw']
-         del jsonObject['subdivisions']
-         del jsonObject['_locales']
-         del jsonObject['maxmind']
-         jsonObject['accept_language'] = self.request.headers.get('Accept-Language')
-         jsonObject['service'] = {} 
-         jsonObject['service']['checked_ip'] = checkIp
-         jsonObject['service']['name'] = "geoip.supermasita.com"
-         jsonObject['service']['description'] = "Get geo IP information based on Maxmind's GeoLite2 data."
-         jsonObject['service']['how_to_use'] = "Connect directly to geoip.supermasita.com to check your request's IP. Use the query string '?ip=' to check any IP. Output is always JSON."
-         jsonObject['service']['license'] = "Attribution-ShareAlike 4.0 International (CC BY-SA 4.0).This product includes GeoLite2 data created by MaxMind, available from http://www.maxmind.com"
+
+         # IP info
+         jsonGeoip = {}	
+         jsonGeoip = vars(response)
+         del jsonGeoip['raw']
+         del jsonGeoip['maxmind']
+         jsonGeoip['service'] = {} 
+         jsonGeoip['service']['checked_ip'] = checkIp
+         jsonGeoip['service']['name'] = "geoip.supermasita.com"
+         jsonGeoip['service']['description'] = "Get geo IP information based on Maxmind's GeoLite2 data."
+         jsonGeoip['service']['how_to_use'] = "Connect directly to geoip.supermasita.com to check your request's IP. Use the query string '?ip=' to check any IP. Output is always JSON."
+         jsonGeoip['service']['license'] = "Attribution-ShareAlike 4.0 International (CC BY-SA 4.0).This product includes GeoLite2 data created by MaxMind, available from http://www.maxmind.com"
+      
+         # Review this header workaround 
+         self.set_header('Content-Type', 'application/json') 
+         self.finish(json.dumps(jsonGeoip, default=lambda o: o.__dict__))
          
-         self.write(json.dumps(jsonObject, default=lambda o: o.__dict__))
-         
-         reader.close()
 
 def make_app():
     return tornado.web.Application([
