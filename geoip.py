9bba5c793dc8457838e4b2b08ef8af8a9265a22a
root <root@super02.supermasita.com>
2018-04-20 02:07:48 +0000

    after reading a book

--- geoip.py
+++ geoip.py
@@ -6,40 +6,66 @@ import ipaddress
 
 geoipDbPath = '/opt/geoip.supermasita.com/GeoLite2-City/GeoLite2-City.mmdb'
 
-class MainHandler(tornado.web.RequestHandler):
+class ip():
+
+    def get_geo(remote_ip):
+         
+         dbReader = geoip2.database.Reader(geoipDbPath)
+         try:
+             response = dbReader.city(remote_ip)
+             return response
+         except:
+             raise ValueError
+         finally:
+             dbReader.close()
+
+    def is_ip(remote_ip):
+         # IP or not? 
+         try:
+             return ipaddress.IPv4Address(remote_ip)
+         #except ipaddress.AddressValueError:
+         except:
+             raise ValueError
+
+
+class jsonHandler(tornado.web.RequestHandler):
+
     def get(self):
 
          # Checking remote_ip or query string?
-         queryIp = self.get_arguments('ip', strip=True)
+         if self.get_arguments('ip'):
+             remote_ip = self.get_arguments('ip', strip=True)
+             remote_ip = remote_ip[0]
+         elif self.request.headers.get("X-Real-IP") :
+             remote_ip = self.request.headers.get("X-Real-IP") 
+         elif self.request.headers.get("X-Forwarded-For") :
+             remote_ip = self.request.headers.get("X-Forwarded-For") 
+         else:
+             remote_ip = self.request.remote_ip 
 
-         if queryIp :
-             checkIp = queryIp[0]
-             # IP or not? 
-             try:
-                 ipaddress.IPv4Address(checkIp)
-             except ipaddress.AddressValueError:
-                 self.set_status(400)
-                 jsonErrorMsg = { 'status': 400, 'description': 'Not a valid IP'}
-                 self.finish(jsonErrorMsg)
-                 return
-         else :
-             checkIp = self.request.remote_ip 
+         #
+         user_agent = self.request.headers.get("User-Agent")
 
-
-         # Load DB
-         dbReader = geoip2.database.Reader(geoipDbPath)
-
-        
-         # Check if exists in DB 
+         # IP or not? 
          try:
-             response = dbReader.city(checkIp)
-         except geoip2.errors.AddressNotFoundError:
+             ip.is_ip(remote_ip)
+         #except ipaddress.AddressValueError:
+         except ValueError:
              self.set_status(400)
-             jsonErrorMsg = { 'status': 400, 'description': 'IP not found in GeoLite2 database'}
+             jsonErrorMsg = { 'status': 400, 'description': 'Not a valid IP'}
              self.finish(jsonErrorMsg)
              return
-         finally:
-             dbReader.close()
+
+
+         # Check if exists in DB 
+         try:
+             response = ip.get_geo(remote_ip)
+         #except geoip2.errors.AddressNotFoundError:
+         except ValueError:
+             self.set_status(400)
+             jsonErrorMsg = { 'status': 400, 'description': 'IP not found in GeoLite2 database1'}
+             self.finish(jsonErrorMsg)
+             return
 
 
          # IP info
@@ -47,21 +73,69 @@ class MainHandler(tornado.web.RequestHandler):
          jsonGeoip = vars(response)
          del jsonGeoip['raw']
          del jsonGeoip['maxmind']
-         jsonGeoip['service'] = {} 
-         jsonGeoip['service']['checked_ip'] = checkIp
-         jsonGeoip['service']['name'] = "geoip.supermasita.com"
-         jsonGeoip['service']['description'] = "Get geo IP information based on Maxmind's GeoLite2 data."
-         jsonGeoip['service']['how_to_use'] = "Connect directly to geoip.supermasita.com to check your request's IP. Use the query string '?ip=' to check any IP. Output is always JSON."
-         jsonGeoip['service']['license'] = "Attribution-ShareAlike 4.0 International (CC BY-SA 4.0).This product includes GeoLite2 data created by MaxMind, available from http://www.maxmind.com"
       
          # Review this header workaround 
          self.set_header('Content-Type', 'application/json') 
-         self.finish(json.dumps(jsonGeoip, default=lambda o: o.__dict__))
+         self.set_header('Max-age', '3600')
+ 
+         self.write(json.dumps(jsonGeoip, default=lambda o: o.__dict__))
+         
+
+class MainHandler(tornado.web.RequestHandler):
+
+    def get(self):
+
+         # Checking remote_ip or query string?
+         if self.get_arguments('ip'):
+             remote_ip = self.get_arguments('ip', strip=True)
+             remote_ip = remote_ip[0]
+         elif self.request.headers.get("X-Real-IP") :
+             remote_ip = self.request.headers.get("X-Real-IP") 
+         else:
+             remote_ip = self.request.remote_ip 
+
+         #
+         user_agent = self.request.headers.get("User-Agent")
+
+         # IP or not? 
+         try:
+             ip.is_ip(remote_ip)
+         #except ipaddress.AddressValueError:
+         except ValueError:
+             self.set_status(400)
+             jsonErrorMsg = { 'status': 400, 'description': 'Not a valid IP'}
+             self.finish(jsonErrorMsg)
+             return
+
+
+         # Check if exists in DB 
+         try:
+             response = ip.get_geo(remote_ip)
+         #except geoip2.errors.AddressNotFoundError:
+         except ValueError:
+             self.set_status(400)
+             jsonErrorMsg = { 'status': 400, 'description': 'IP not found in GeoLite2 database1'}
+             self.finish(jsonErrorMsg)
+             return
+
+
+         # IP info
+         jsonGeoip = {}	
+         jsonGeoip = vars(response)
+         del jsonGeoip['raw']
+         del jsonGeoip['maxmind']
+      
+         # Review this header workaround 
+         self.set_header('Content-Type', 'application/json') 
+         self.set_header('Max-age', '3600')
+
+         self.write(json.dumps(jsonGeoip, default=lambda o: o.__dict__))
          
 
 def make_app():
     return tornado.web.Application([
-        (r"/.*", MainHandler),
+        (r"/", MainHandler),
+        (r"/json/.*", jsonHandler),
     ])
 
 if __name__ == "__main__":
